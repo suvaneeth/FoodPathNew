019e7cc3ed2a84e0bc36a85e26935df31ff0f114
suvaneeth <suvaneeth@gmail.com>
2013-11-15 20:45:09 +0300

    first commit

---
+++ FoodPath.js
@@ -0,0 +1,17 @@
+// Set the initial Lat and Long of the Google Map
+
+function createLoc(){/*
+	var myLatLng = new google.maps.LatLng(25.280468, 51.522312);
+
+	// Google Map options
+	var myOptions = {
+      zoom: 15,
+      center: myLatLng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    // Create the Google Map, set options
+    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);*/
+   
+   window.location.href = '#Menu'
+   }
